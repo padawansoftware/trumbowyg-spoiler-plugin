6a30bc3f37ad5528d5479eb23d480c87321e5f9e
Álvaro Cebrián <alvaro.cebrian.garcia@gmail.com>
2019-11-04 12:47:23 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+An spoiler plugin for trumbowyg based on [spoiler.js](https://github.com/padawansoftware/spoiler.js).
+
+Let you easely create spoilers in [trumbowyg](https://github.com/Alex-D/Trumbowyg) WYSIWYG editor.
---
+++ trumbowyg.spoiler.js
@@ -0,0 +1,68 @@
+Spoiler = require('@padawansoftware/spoiler.js');
+
+(function ($, Spoiler) {
+    'use strict';
+
+    function buildButtonDef(trumbowyg) {
+        return {
+            fn: function () {
+                var $modal = trumbowyg.openModal('Spoiler', [
+                    `<div class="${trumbowyg.o.prefix}highlight-form-group ">
+                        <label>Title</label>
+                        <input id="title" class="${trumbowyg.o.prefix}highlight-form-control title"></input>
+                    </div>
+                    <div class="${trumbowyg.o.prefix}highlight-form-group">
+                        <label>Content</label>
+                       <textarea class="${trumbowyg.o.prefix}highlight-form-control content"></textarea>
+                    </div>
+                    `
+                ].join('\n')),
+
+                $title = $modal.find('.title'),
+                $content = $modal.find('.content');
+
+                // Listen clicks on modal box buttons
+                $modal.on('tbwconfirm', function () {
+                    trumbowyg.restoreRange();
+                    trumbowyg.execCmd('insertHTML', Spoiler.create({
+                        'spoilerText': $title.val(),
+                        'spoilerContent': $content.val() ? $content.val() : (trumbowyg.getRangeText() ? trumbowyg.getRangeText() : '<br/>')
+                        })
+                    );
+                    trumbowyg.execCmd('insertHTML', '<p><br></p>');
+                    trumbowyg.closeModal();
+
+                    Spoiler.initAll();
+                });
+
+                $modal.on('tbwcancel', function () {
+                    trumbowyg.closeModal();
+                });
+            },
+            title: 'Insert spoiler',
+            text: '[]',
+            hasIcon: false
+        };
+    }
+
+    $.extend(true, $.trumbowyg, {
+        langs: {
+            en: {
+                spoiler: 'Add spoiler'
+            }
+        },
+        plugins: {
+            spoiler: {
+                init: function (trumbowyg) {
+                    trumbowyg.o.plugins.spoiler = $.extend(true, {},
+                        defaultOptions,
+                        trumbowyg.o.plugins.spoiler || {}
+                    );
+
+                    trumbowyg.addBtnDef('spoiler', buildButtonDef(trumbowyg));
+                }
+            }
+        }
+    });
+})(jQuery, Spoiler);
+
